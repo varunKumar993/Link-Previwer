97d71a128d22adcbceaac3aa332c93c70026f2e3
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2025-05-25 22:28:33 +0530

    new component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import LinkPreviewer from "./LinkPreviewer";
 
 function App() {
-  return <div>Link Previewer</div>;
+  return (
+    <div className="container">
+      <div> checkout my </div>
+      <LinkPreviewer url="#">
+        <span className="link">Instagram</span>
+      </LinkPreviewer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/LinkPreviewer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LinkPreviewer = () => {
+  return <div></div>;
+};
+
+export default LinkPreviewer;
