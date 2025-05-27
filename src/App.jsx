c6b283ef0f3bf608360ba9465032e94266067299
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2025-05-27 22:26:07 +0530

    code added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,26 @@ import LinkPreviewer from "./LinkPreviewer";
 function App() {
   return (
     <div className="container">
-      <div> checkout my </div>
-      <LinkPreviewer url="#">
-        <span className="link">Instagram</span>
-      </LinkPreviewer>{" "}
-      Profile
+      <div>
+        {" "}
+        checkout my
+        <LinkPreviewer url="#">
+          <span className="link">Instagram</span>
+        </LinkPreviewer>{" "}
+        Profile{" "}
+      </div>
       <div>
         And my
         <LinkPreviewer url="#">
           <span className="link">Github</span>
         </LinkPreviewer>{" "}
+      </div>
+      <div>
+        And my
+        <LinkPreviewer url="#">
+          <span className="link">Website</span>
+        </LinkPreviewer>{" "}
+        Website
       </div>
     </div>
   );
